adf948bc800a982544894b91b96c19b60c9938ca
riribagz <bagot.ryan26@gmail.com>
2023-11-25 14:24:42 +0100

    feat: lab4

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-const express = require('express')
+const express = require("express");
 
-const app = express()
-const port = 3000
+const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello world!')
-})
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello world!");
+});
 
 module.exports = app.listen(port, (err) => {
-  if (err) throw err
-  console.log("Server listening the port " + port)
-})
+  if (err) throw err;
+  console.log("Server listening on port " + port);
+});
