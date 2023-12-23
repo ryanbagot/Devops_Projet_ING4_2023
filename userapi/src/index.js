a62f3b8610306b2cd7af968dc81848491167b9f9
riribagz <bagot.ryan26@gmail.com>
2023-12-23 13:55:31 +0400

    Feat: Add Dockerfile and create k8s deployment and services

--- userapi/src/index.js
+++ userapi/src/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const userRouter = require('./routes/user')
 const bodyParser = require('body-parser')
 
+
 const app = express()
 const port = process.env.PORT || 3000
 
