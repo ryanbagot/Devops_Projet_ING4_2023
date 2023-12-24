2086bf6a47b0d940c7f45cd90c7d540b9622f3a0
riribagz <bagot.ryan26@gmail.com>
2023-12-24 15:02:44 +0400

    Feat: add some styles

--- index.js
+++ index.js
@@ -4,8 +4,19 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
+app.use(express.static('public'));
+
 app.get("/", (req, res) => {
-  res.send("Hello world!");
+  res.send(`
+    <html>
+      <head>
+        <link rel="stylesheet" type="text/css" href="/styles.css">
+      </head>
+      <body>
+        <h1>Bonjour Monsieur Gonzalo, bonjour Monsieur Bariteau !</h1>
+      </body>
+    </html>
+  `);
 });
 
 module.exports = app.listen(port, (err) => {
