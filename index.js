f658f761f59eab37a187efe7f830c6118cb5bd1c
riribagz <bagot.ryan26@gmail.com>
2023-12-24 15:27:59 +0400

    Feat: add some styles

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.send(`
@@ -14,6 +14,32 @@ app.get("/", (req, res) => {
       </head>
       <body>
         <h1>Bonjour Monsieur Gonzalo, bonjour Monsieur Bariteau !</h1>
+        <div id="bottom-container">
+          <button id="increment">Plus !</button>
+          <p id="value">0</p>
+        </div>
+        <div id="sidebar">
+          <h2>Outils utilisés</h2>
+          <ul>
+            <li>Node.js</li>
+            <li>Express</li>
+            <li>Git</li>
+            <li>GitHub</li>
+            <li>Azure</li>
+            <li>Vagrant</li>
+            <li>Ansible</li>
+            <li>Docker</li>
+            <li>k8s</li>
+            <li>Graffana</li>
+            <li>Prométheus</li>
+          </ul>
+        </div>
+        <script>
+          document.getElementById('increment').addEventListener('click', function() {
+            let value = document.getElementById('value');
+            value.textContent = parseInt(value.textContent) + 1;
+          });
+        </script>
       </body>
     </html>
   `);
